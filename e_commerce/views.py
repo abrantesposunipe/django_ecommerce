ca7211b2a8f56042a9b7de79dabad176ec269211
Rodolfo Abrantes <abrantes.posunipe@gmail.com>
2018-12-09 10:34:14 -0300

    create about and contact pages

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        'title': 'Pagina principal',
+        'content': 'Bem vindo a página principal'
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        'title': 'Pagina sobre',
+        'content': 'Bem vindo a página sobre'
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        'title': 'Pagina de contato',
+        'content': 'Bem vindo a página de contato'
+    }
+    return render(request, "contact/view.html", context)
